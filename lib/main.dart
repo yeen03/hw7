41a6027ddbebbf01f75d2a70e24aec49eb880c72
yeen03 <yeenyeen473@gmail.com>
2022-08-30 13:41:22 +0700

    kk

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+ void main() {
+   runApp(MyApp());
+ }
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+      body: Column(),
+    ),
+    );
+    
+  }
+}
+  
+  
